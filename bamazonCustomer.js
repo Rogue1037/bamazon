87b3838c8fa0dc0f7cc026a26f642fb57631aba6
Dylan Davidson <rogue1037@gmail.com>
2019-04-14 23:04:06 -0700

    added to JavaScript

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,39 +19,65 @@ connection.connect(function (err) {
 });
 
 function start() {
-
+    console.log("Welcome to BamaZON!!");
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
         inquirer
             .prompt([
                 {
                     name: "choice",
-                    type: "list",
+                    type: "rawlist",
                     choices: function () {
                         var choiceArray = [];
                         for (var i = 0; i < results.length; i++) {
-                            choiceArray.push(results[i].product_name);
+                            choiceArray.push(results[i].product_name + " " + results[i].price + " " + results[i].department_name + " " + results[i].stock_quantity);
                         }
-                        
+
                         return choiceArray;
 
                     },
-                    message: "What product id would you like to buy?"
+                    message: "What product would you like to buy?"
                 },
                 {
-                    name: "product_id",
-                    type: "input",
+                    name: "product",
+                    type: "number",
                     message: "How many would you like?"
                 }
             ])
             .then(function (answer) {
 
-                var chosenProductID;
+                var chosenItem = [];
                 for (var i = 0; i < results.length; i++) {
-                    if (results[i].product_name === answer.choice) {
-                        chosenProductID = results[i];
+                    if (results[i].stock_quanity === answer.choice) {
+                        chosenItem = results[i];
+                        console.log(chosenItem);
                     }
                 }
+
+                if (chosenItem > parseInt(answer.stock_quanity)) {
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quanity: - 1
+                            },
+                            {
+                                id: results[i].item_id
+                            }
+                        ],
+                        function (error) {
+                            if (error) throw err;
+                            console.log("purchased!");
+                            start();
+                        }
+                    );
+                }
+                else {
+                    console.log("sold out. Please look at our other items.");
+                
+                }
+                start();
+                
             });
     });
 }
