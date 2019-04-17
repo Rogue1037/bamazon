a18373e6d643e7fde3d7866e7226b9be6df91bff
Dylan Davidson <rogue1037@gmail.com>
2019-04-16 20:36:49 -0700

    fixed issue with prompting inventory. new throw that is killing me again.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,42 +13,51 @@ var connection = mysql.createConnection({
     database: "bamazonDB"
 });
 
+function validateInput(value) {
+    var integer = Number.isInteger(parseFloat(value));
+    var sign = Math.sign(value);
+
+    if (integer && (sign === 1)) {
+        return true;
+    } else {
+        return 'Whole numbers only please.';
+    }
+}
+
 connection.connect(function (err) {
     if (err) throw err;
-    start();
+    displayInventory();
 });
 
 function start() {
     console.log("Welcome to BamaZON!!");
     connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
-        inquirer
-            .prompt([
-                {
-                    name: "choice",
-                    type: "rawlist",
-                    choices: function () {
-                        var choiceArray = [];
-                        for (var i = 0; i < results.length; i++) {
-                            choiceArray.push("Item ID: " + results[i].item_id + " " + "Product Name: " + results[i].product_name + " " + "$" + results[i].price + " " + "Dept: " + results[i].department_name + " " + "Inventory: " + results[i].stock_quantity);
-                        }
+        inquirer.prompt([
+            {
+                name: "choice",
+                type: "input",
+                message: "What product would you like to buy?",
+                validate: validateInput,
+                filter: Number
 
-                        return choiceArray;
+            },
+            {
+                name: "product",
+                type: "input",
+                message: "How many would you like?",
+                validate: validateInput,
+                filter: Number
 
-                    },
-                    message: "What product would you like to buy?"
-                },
-                {
-                    name: "product",
-                    type: "number",
-                    message: "How many would you like?"
-                }
-            ])
-            .then(function (answer) {
+            }
+        ])
+            .then(function (input) {
 
-                var item = answer.choice;
-                console.log(item);
-                var quantity = answer.product;
+                var item = input.choice;
+               
+                var quantity = input.product;
+              
+
 
 
                 var queryInventory = 'SELECT * FROM products WHERE ?';
@@ -60,16 +69,21 @@ function start() {
                         start();
                     } else {
                         var productData = data[0];
-
+                        
 
                         if (quantity <= productData.stock_quantity) {
                             console.log('Awesome, we will ship it out right away!');
 
 
                             var updateQueryInventory = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE product_name = ' + item;
+                            
+
 
                             connection.query(updateQueryInventory, function (err, data) {
+
+
                                 if (err) throw err;
+
 
                                 console.log('Your total is $' + productData.price * quantity);
                                 console.log('Thank you for shopping with us!');
@@ -90,6 +104,29 @@ function start() {
     });
 
 }
+function displayInventory() {
+
+    queryStr = 'SELECT * FROM products';
+    connection.query(queryStr, function (err, data) {
+        if (err) throw err;
+
+        console.log('Existing Inventory: ');
+        var strOut = '';
+        for (var i = 0; i < data.length; i++) {
+            strOut = '';
+            strOut += 'Item ID: ' + data[i].item_id + '  //  ';
+            strOut += 'Product Name: ' + data[i].product_name + '  //  ';
+            strOut += 'Department: ' + data[i].department_name + '  //  ';
+            strOut += 'Price: $' + data[i].price + '\n';
+
+            console.log(strOut);
+        }
+        start();
+    });
+}
+
+
+
 
 
 
